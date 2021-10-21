fda24d3fcf4f05aac4cdbdffeb5ab5ce59ff1cf3
Vandeilsonpp <vandeilson.nobre@picpay.com>
2021-10-20 22:33:59 -0300

    feito configuração do kafka de acordo com o curso da udemy

--- src/main/java/com/vandeilson/kafka/controller/FirstController.java
+++ src/main/java/com/vandeilson/kafka/controller/FirstController.java
@@ -1,15 +1,18 @@
 package com.vandeilson.kafka.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
+@Slf4j
 public class FirstController {
 
     @GetMapping
     public String getHelloWorld() {
+        log.info("Entrando no controller e logando com o maior sucesso do mundo");
         return "Hello, world!";
     }
 
