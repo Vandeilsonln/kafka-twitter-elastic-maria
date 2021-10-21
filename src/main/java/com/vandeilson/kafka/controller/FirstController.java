81aa9140fbac963bd36fb6173ce6f1088d7db19e
Vandeilsonpp <vandeilson.nobre@picpay.com>
2021-10-21 17:48:42 -0300

    Criado um endpoint que envia records com keys

--- src/main/java/com/vandeilson/kafka/controller/FirstController.java
+++ src/main/java/com/vandeilson/kafka/controller/FirstController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 
 @RestController
 @RequestMapping
@@ -18,15 +20,54 @@ public class FirstController {
     public String getHelloWorld() {
         KafkaProducer<String, String> producer = Producers.getProducer();
 
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 0; i <= 1; i++) {
             ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", Integer.toString(i));
-            producer.send(producerRecord);
+            producer.send(producerRecord, (m, e) -> {   // new Callback( RecordMetadata rm, Exception e) -> É uma interface funcional
+                if (e == null) {
+                    log.info("Received new matadata \n" +
+                        "Topic:\t" + m.topic() +
+                        "\nPartition:\t" + m.partition() +
+                        "\nOffset:\t" + m.offset() +
+                        "\nTimestamp:\t" + m.timestamp());
+                } else {
+                    log.error("Error while producing.\n" +
+                        e);
+                }
+            });
+
         }
 
         // flush and close producer
         producer.close();
 
         return "Hello, world!";
+    }
+
+    @GetMapping("/keys")
+    public String producerWithKeys() throws ExecutionException, InterruptedException {
+        KafkaProducer<String, String> producer = Producers.getProducer();
+
+        for (int i = 0; i <= 10; i++) {
+            String value = Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+            log.info("Key:\t" + key);
+
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("new_topic", key, value);
+
+            producer.send(producerRecord, (m, e) -> {   // new Callback( RecordMetadata rm, Exception e) -> É uma interface funcional
+                if (e == null) {
+                    log.info("Received new matadata \n" +
+                        "Topic:\t" + m.topic() +
+                        "\nPartition:\t" + m.partition() +
+                        "\nOffset:\t" + m.offset() +
+                        "\nTimestamp:\t" + m.timestamp() + "\n");
+                } else {
+                    log.error("Error while producing.\n" +
+                        e);
+                }}).get(); // NEVER DO THIS IN PRODUCTION
+        }
+
+        return "Producer with keys";
     }
 
 }
